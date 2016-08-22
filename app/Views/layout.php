b0877fabcc1380400b79a54d53e1c01c0529a23e
Levergor <levergor@gmail.com>
2016-08-22 18:23:14 +0200

    Correction mozaique Accueil

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -17,6 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/vendor/shuffle.css') ?>">
 	
 
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/font-awesome.min.css') ?>">
