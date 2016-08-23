952e89be197da3c5a769402a447f418fc36133df
moni <monica_be@hotmail.fr>
2016-08-23 19:19:16 +0200

    Modis vue Sujet
    Rajout de js et modals

--- app/Views/membre/sujet.php
+++ app/Views/membre/sujet.php
@@ -5,61 +5,87 @@
 
 
 <main class="sujet">
-    <div style="margin: auto; width: 50%;">
-   
-        <img src="<?= $this->assetUrl('/img/'.$sujet->getSujetPhoto()); ?>"><br>
-         <div><?=$sujet->getNomSujet()?><div>
-        <?=$sujet->getDescriptionSujet()?>
-        
+    <!-- Modal content -FENETRE MODAL DE COMMENTAIRE -->
+    <div id="myWinmodal" class="myWinmodal">
+        <div class="myWinmodal-content">                        
+         <p class="closing">Fermer</p>                                               
+            <form id="formCommentsId" name="formComments" class="formulaireComments" method="POST"; ?>
+                <h3>Choisissez votre café de préference</h3>                                                                       
+                <label for="commentaire">Commentaire</label>
+                <textarea name="commentaire" id="commentaireId" rows="5" cols="100"></textarea>
+                <input type="submit" value="Meet up" class="btn">
+            </form>                    
+        </div>            
     </div>
 
-    <div style="margin: 30px 0; width: 100%; background-color: #E3E7E8; color: #919E9E; font-size: 2em; text-align: center; font-family: 'Helvetica', sans serif; text-transform: uppercase; letter-spacing: 0.1em;">
-    <!--it’s a pleasure to meet you.<br>
-        let’s have coffee sometime.<br> -->
-        <button>comment</button><br>
-        <button>meet up</button>
-    </div>
+    <div class="container-liste">
+        <div class="liste-suj-com-salon">                           
+            <img src="<?= $this->assetUrl('/img/'.$sujet->getSujetPhoto()); ?>">
+             <div><?=$sujet->getNomSujet()?></div>
+            <?=$sujet->getDescriptionSujet()?>        
+        </div>
 
-		
-		
-<!-- <?php //print_r($ListeDeSalons); ?> -->
-<h1>Liste des Salons</h1>
- <?php foreach ($ListeDeSalons as $UnSalon):?>
-		<img src="<?= $this->assetUrl('/upload/'.$UnSalon->getPhoto());?>"><br>
-        <?=$UnSalon->getDateRdv()?>
+        <div class="liste-com-suj">
+            <div class="liste-com">           
+                <button id="myBtnLogin"> <?php if(null ==$w_user) : ?>
+                    <p class="msg-error">
+                        <a href="#">Connectez-vous ou créez un Compte</a> 
+                        <br>pour ajouter un commentaire ou participer à un salon !
+                    </p>
+                </button>
+                <?php else : ?>
+                <button id="myBtn2">comment</button>
+                <button>meet up</button>
 
-        <a href="<?= $this->url('salon', ['id_salon' => $UnSalon->getIdSalon()]) ?>"><?=$UnSalon->getLieuRdv()?></a>
-        <?=$UnSalon->getNom()?>
-         <?=$UnSalon->getOpenClose()?> 
-         
-				
-			<?php endforeach; ?>
-<h1>Ecrire un Commentaire</h1>
-            <form method="post">
-                     <textarea name="commentaire" id="commentaire" rows="10" cols="150"></textarea></br>
-                     <input type="submit" value="Envoyer" />
-                </form>
+                <?php endif; ?>                                         
+        </div>  
+
+
+
+		   <section class="liste-left">
+                <h3>Liste des Commentaires</h3>
 
             <?php
-
-            foreach ($affichageCommentaire as $key => $value) {
-                echo '
-                    <div class="membre meetup">
-                        <img style="width:5%;" src="'.$this->assetUrl('/upload/'.$value->getPhoto()).'">
-                        <div class="comments">
-                            <em>' . $value->getPseudo() . '</em><br>
-                           ' . $value->getCommentaireSujet() . '
-                        </div>
-                        <div class="float_right">';
-                if(($value->getAgePost()) == "NEW"){echo '<em style="display: inline-block; height: 17px; padding: 0 5px; background: #DF6C4F; font-size: 13px; font-style: normal; color: #FFF; line-height: 18px; border-radius: 2px;">New</em>';};
-                echo '<p>' . $value->getDateCommentaire() . '
-                        </div>
-                    </div>';
+                foreach ($affichageCommentaire as $key => $value) {
+                
+                echo '<div class="membre-comments">
+                        <img src="'.$this->assetUrl('/upload/'.$value->getPhoto()).'">
+                        <div class="comments-txt">
+                            <h5>' . $value->getPseudo() . '</h5>
+                            <p>' . $value->getCommentaireSujet() . 
+                            '</p></div>
+                            <div class="new-label-container">';
+                if(($value->getAgePost()) == "NEW"){echo '<p class="new-label-new">New</p>';};
+                echo '<p class="new-label-date">' . $value->getDateCommentaire() . '</p>
+                       </div>
+                    </div><hr>';
             }
-
-
             ?>
+            </section>
+                
+         <section class="liste-rigth">
+                <h3>Liste des Salons</h3>
+                    <div class="membre-comments">
+                        <h5> <?php foreach ($ListeDeSalons as $UnSalon):?></h5>
+                		<img src="<?= $this->assetUrl('/upload/'.$UnSalon->getPhoto());?>">
+                            <div class="new-label-container-salon">
+                            <p class="new-label-date"><?=$UnSalon->getDateRdv()?></p>
+                            
+                            <a href="<?= $this->url('salon', ['id_salon' => $UnSalon->getIdSalon()]) ?>"><?=$UnSalon->getLieuRdv()?></a>
+                            </div>
+                        <div class="comments-txt-salon">   
+                            <?=$UnSalon->getNom()?>
+                            <?=$UnSalon->getOpenClose()?> 				        			     
+                        </div>
+                        <?php endforeach; ?>
+                        
+                    </div>
+                    <hr>
+            </section> 
+        </div> 
+    </div>
 
 </main>
+
 
 <?php $this->stop('main_content') ?>
